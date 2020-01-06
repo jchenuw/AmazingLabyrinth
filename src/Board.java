c58671dc607b0a9113898422492a8db0b8551840
Howard Hong <howard.hong@mail.utoronto.ca>
2020-01-06 18:07:54 -0500

    Finished row/column shifting methods for Board.java.

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,7 @@
 public class Board {
 
 	// Board tiles
+	// Goes from left to right, top to bottom.
 	private Tile[][] tiles;
 
 	// Players
@@ -10,18 +11,30 @@ public class Board {
 	private Tile extraTile;
 
 	public void shiftRowLeft(Integer row) {
-		// TODO: complete this function.
+		Tile newExtraTile = tiles[row][0];
+		System.arraycopy(tiles[row], 1, tiles[row], 0, tiles[row].length - 1);
+		tiles[row][tiles.length - 1] = extraTile;
+		extraTile = newExtraTile;
 	}
 
 	public void shiftRowRight(Integer row) {
-		// TODO: complete this function.
+		Tile newExtraTile = tiles[row][tiles.length - 1];
+		System.arraycopy(tiles[row], 0, tiles[row], 1, tiles[row].length - 1);
+		tiles[row][0] = extraTile;
+		extraTile = newExtraTile;
 	}
 
 	public void shiftColUp(Integer col) {
-		// TODO: complete this function.
+		Tile newExtraTile = tiles[0][col];
+		System.arraycopy(tiles, 1, tiles, 0,  tiles.length - 1);
+		tiles[tiles.length - 1][col] = extraTile;
+		extraTile = newExtraTile;
 	}
 
 	public void shiftRowDown(Integer col) {
-		// TODO: complete this function.
+		Tile newExtraTile = tiles[tiles.length - 1][col];
+		System.arraycopy(tiles, 0, tiles, 1,  tiles.length - 1);
+		tiles[0][col] = extraTile;
+		extraTile = newExtraTile;
 	}
 }
