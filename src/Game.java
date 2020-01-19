c7d834093648fc4934d650c11fa1c8f5b75c5b2e
Jyaer <cje9507@gmail.com>
2020-01-18 20:09:20 -0500

    Added turnState inner class in Game to hold turn-related variables. Added Card distribution setup.

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,49 @@
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
 public class Game {
+
+	/**
+	 * Enscapulation class to hold current turn data to enforce rules
+	 */
+	public class TurnState {
+		private int playerTurn;
+		private boolean insertedTile;
+		private boolean moved;
+		private boolean gameEnd = false;
+
+		private TurnState(int playerTurn, boolean insertedTile, boolean moved) {
+			this.playerTurn = playerTurn;
+			this.insertedTile = insertedTile;
+			this.moved = moved;
+		}
+		// Getters
+		public int getPlayerTurn() {
+			return this.playerTurn;
+		}
+		public void setPlayerTurn(int playerTurn) {
+			this.playerTurn = playerTurn;
+		}
+
+		public boolean hasInsertedTile() {
+			return this.insertedTile;
+		}
+		public void setInsertedTile(boolean insertedTile) {
+			this.insertedTile = insertedTile;
+		}
+
+		public boolean hasMoved() {
+			return moved;
+		}
+		public void setMoved(boolean moved) {
+			this.moved = moved;
+		}
+
+		public boolean hasGameEnd() {
+			return this.gameEnd;
+		}
+	}
 
     // Game board
     private Board board;
@@ -9,7 +52,7 @@ public class Game {
 	private Card[] cards;
 
     // Players of the game
-	private ArrayList<Player> players = new ArrayList<Player>();
+	private Player[] players;
 	private final int numPlayers;
 
 	// Treasures
@@ -18,7 +61,7 @@ public class Game {
 	// Extra insertable tile
 	private Tile extraTile;
 
-	// Game-state variables
+	private TurnState turnState;
 
 	/**
 	 * Creates a new game that initializes a new board
@@ -29,6 +72,23 @@ public class Game {
 		this.numPlayers = numPlayers;
 
 		init();
+
+		turnState = new TurnState(1, false, false);
+	}
+
+	public void endGame() {
+		turnState.gameEnd = true;
+	}
+
+	public void nextTurn() {
+		turnState.playerTurn++;
+
+		if(turnState.playerTurn > players.length) {
+			turnState.playerTurn = 0;
+		}
+
+		turnState.insertedTile = false;
+		turnState.moved = false;
 	}
 
 	/**
@@ -39,6 +99,7 @@ public class Game {
 		// setup pre-game components
 		setupPlayers();
 		setupCardsAndTreasures();
+		distributeCards();
 
 		// initialize new board
 		board = new Board(this.players, this.treasures);
@@ -49,20 +110,22 @@ public class Game {
 	/**
 	 * Initializes players for the game
 	 */
-	public void setupPlayers() {
+	private void setupPlayers() {
+		players = new Player[numPlayers];
+
 		// preset player data
 		String[] colours =  {"Red", "Blue", "Green", "Purple"};
 		int[][] startingPoints = {{0,0}, {6, 0}, {0, 6}, {6, 6}};
 
 		for(int i = 0; i < numPlayers; i++) {
-			players.add(new HumanPlayer(startingPoints[i][0], startingPoints[i][1], colours[i]));
+			players[i] = new HumanPlayer(startingPoints[i][0], startingPoints[i][1], colours[i]);
 		}
 	}
 
 	/**
 	 * Initializes treasures and treasure cards
 	 */
-	public void setupCardsAndTreasures() {
+	private void setupCardsAndTreasures() {
 		// create treasures and treasure cards
 		treasures = new Treasure[Treasure.TREASURE_AMOUNT];
 		cards = new Card[Treasure.TREASURE_AMOUNT];
@@ -73,14 +136,34 @@ public class Game {
 		}
 	}
 
-	/**
-	 * Checks if a player has won by checking if they have collected all the treasure
-	 *
-	 * @param player is The player to the checked
-	 * @return if the player has won
-	 */
-	public boolean isWinner(Player player){
-		return(player.isWon());
+	private void distributeCards() {
+		Card[] shuffledCards = new Card[cards.length];
+
+		Random rand = new Random();
+
+		// Fisher-Yates shuffle
+		for(int i = shuffledCards.length - 1; i > 0; i--) {
+			int index = rand.nextInt(i + 1);
+
+			// swap
+			Card temp = shuffledCards[index];
+			shuffledCards[index] = shuffledCards[i];
+			shuffledCards[i] = temp;
+		}
+
+		int cardsPerPlayer = 5;
+		int cardCounter = 0;
+
+		for(int i = 0; i < players.length; i++) {
+			Stack<Card> newHand = new Stack<Card>();
+
+			for(int j = 0; j < cardsPerPlayer; j++) {
+				newHand.push(shuffledCards[cardCounter]);
+				cardCounter++;
+			}
+
+			players[i].setHand(newHand);
+		}
 	}
 
 	// Getters
@@ -98,6 +181,10 @@ public class Game {
 
 	public Tile getExtraTile() {
 		return this.extraTile;
+	}
+
+	public TurnState getTurnState() {
+		return this.turnState;
 	}
 
 }
