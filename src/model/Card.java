0cc222e37909290e9c2b7d6e6ffe7c3dc5326355
Jyaer <cje9507@gmail.com>
2020-01-19 20:46:37 -0500

    Removed unnecessary card fields

--- src/model/Card.java
+++ src/model/Card.java
@@ -5,17 +5,9 @@ public class Card {
 	// model.Treasure to collect for this card
 	private Treasure treasure;
 
-	// If the treasure on the card has been collected
-	private boolean isCollected = false;
-
-
 	// Constructor
 	public Card(Treasure treasure) {
 		this.treasure = treasure;
-	}
-
-	public void complete() {
-		isCollected = true;
 	}
 
 	// Setters and getters
