185da038e56d5fad6400f0a2a415a3ed129875dd
Jyaer <cje9507@gmail.com>
2020-01-23 03:48:47 -0500

    Documentation update

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,10 +1,31 @@
+/**
+ * The Amazing Labyrinth is a Java implementation of Labyrinth the
+ * board game.
+ *
+ * Game board is formed by stationary and moving tiles of different openings,
+ * thus creating a maze. There is always an extra tile, which the player can insert
+ * using any of the arrows. Once the extra tile is inserted, the row or column
+ * shifts and the tile that is pushed out becomes the extra tile. The player can
+ * then click on a tile to move there, as long as the path is valid.
+ *
+ * The goal of the game is to collect all the treasures in the maze based on
+ * the treasure cards shuffled and given to you.
+ *
+ * Rules:
+ * - Must insert the extra tile at beginning of turn, before you move
+ *   (Note the extra tile can be rotated to your advantage)
+ *
+ * Areas of Improvement:
+ * - Better AI
+ * - Save / Load
+ *
+ * @author Jier
+ */
 public class Launcher {
     public static void main(String[] args) {
 
-        // model.Game menu
+        // Game menu
         Menu labyrinthMenu = new Menu();
 
-        // Testing the game
-        //model.Game labyrinthGame = new model.Game(4);
     }
 }
